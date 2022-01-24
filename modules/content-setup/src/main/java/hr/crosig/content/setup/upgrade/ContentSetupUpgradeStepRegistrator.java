e98b81ef9577d58af197f59232c9cddd5f09f85d
Guilherme Kfouri <guiokfouri@gmail.com>
2022-01-24 16:02:12 -0300

    CAP-98 Create content-setup module

---
+++ modules/content-setup/src/main/java/hr/crosig/content/setup/upgrade/ContentSetupUpgradeStepRegistrator.java
@@ -0,0 +1,13 @@
+package hr.crosig.content.setup.upgrade;
+
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+import org.osgi.service.component.annotations.Component;
+
+@Component(immediate = true, service = UpgradeStepRegistrator.class)
+public class ContentSetupUpgradeStepRegistrator implements UpgradeStepRegistrator {
+
+    @Override
+    public void register(Registry registry) {
+
+    }
+}
