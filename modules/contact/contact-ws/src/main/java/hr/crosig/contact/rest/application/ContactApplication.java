4405b215d59a0088198b40569fa4a0b32cce3e91
Victor Catanante <victor.catanante@objective.com.br>
2022-01-26 11:47:44 -0300

    CAP-114 Solve the HttpRequest dependency problem

--- modules/contact/contact-ws/src/main/java/hr/crosig/contact/rest/application/ContactApplication.java
+++ modules/contact/contact-ws/src/main/java/hr/crosig/contact/rest/application/ContactApplication.java
@@ -1,8 +1,6 @@
 package hr.crosig.contact.rest.application;
 
 import hr.crosig.contact.rest.application.utils.ApiConstants;
-import hr.crosig.contact.rest.application.utils.HttpRequest;
-import hr.crosig.contact.rest.application.utils.WebserviceUtils;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.jaxrs.whiteboard.JaxrsWhiteboardConstants;
 
@@ -10,11 +8,14 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.Collections;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author victor.catanante
@@ -23,20 +24,21 @@ import java.util.Set;
         property = {
                 JaxrsWhiteboardConstants.JAX_RS_APPLICATION_BASE + "=/agent-portal",
                 JaxrsWhiteboardConstants.JAX_RS_NAME + "=Contact.Rest",
-                "auth.verifier.guest.allowed=false",
-                "liferay.access.control.disable=false"
+                JaxrsWhiteboardConstants.JAX_RS_MEDIA_TYPE + "=application/json",
+                "auth.verifier.guest.allowed=true",
+                "liferay.access.control.disable=true"
         },
         service = Application.class
 )
-@Produces(MediaType.APPLICATION_JSON)
-@Path("/contacts")
 public class ContactApplication extends Application {
+
     public Set<Object> getSingletons() {
         return Collections.singleton(this);
     }
 
     @GET
     @Path("/search")
+    @Produces(MediaType.APPLICATION_JSON)
     public Response search(@QueryParam("q") String query,
                            @QueryParam("city") String city,
                            @QueryParam("address") String address,
@@ -45,13 +47,24 @@ public class ContactApplication extends Application {
                            @QueryParam("phoneNumber") String phoneNumber,
                            @QueryParam("email") String email) {
         try {
-            String response = new HttpRequest().sendRequestWithResponse(ApiConstants.MOCK_CONTACTS_API_URL, null, ApiConstants.METHOD.GET);
-
-            return WebserviceUtils.successResponse(response);
+            return getClient()
+                        .target(ApiConstants.MOCK_CONTACTS_API_URL)
+                        .request()
+                        .get();
         } catch (Exception e) {
-            return WebserviceUtils.errorResponse(e);
+            return Response.serverError().build();
         }
     }
 
+    private Client getClient() {
+        if (client == null) {
+            ClientBuilder clientBuilder = ClientBuilder.newBuilder();
+            clientBuilder.connectTimeout(10, TimeUnit.SECONDS);
+            clientBuilder.readTimeout(60, TimeUnit.SECONDS);
+            client = clientBuilder.build();
+        }
+        return client;
+    }
 
+    private Client client;
 }
