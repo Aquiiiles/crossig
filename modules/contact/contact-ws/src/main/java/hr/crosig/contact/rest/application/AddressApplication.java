e79469c8d17fb57c744361453d565be585dc8a2e
mmazurky <mmazurky@gmail.com>
2022-01-26 17:53:17 -0300

    CAP-121 Add a rest application module boilerplate for Address endpoints

---
+++ modules/contact/contact-ws/src/main/java/hr/crosig/contact/rest/application/AddressApplication.java
@@ -0,0 +1,56 @@
+package hr.crosig.contact.rest.application;
+
+import hr.crosig.contact.rest.application.utils.ApiConstants;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.jaxrs.whiteboard.JaxrsWhiteboardConstants;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.Collections;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author marcelo.mazurky
+ */
+@Component(
+        property = {
+                JaxrsWhiteboardConstants.JAX_RS_APPLICATION_BASE + "=/agent-portal/city",
+                JaxrsWhiteboardConstants.JAX_RS_NAME + "=City.Rest",
+                JaxrsWhiteboardConstants.JAX_RS_MEDIA_TYPE + "=application/json",
+                "auth.verifier.guest.allowed=true",
+                "liferay.access.control.disable=true"
+        },
+        service = Application.class
+)
+public class AddressApplication extends Application {
+
+    private Client _client;
+
+    public Set<Object> getSingletons() {
+        return Collections.singleton(this);
+    }
+
+    
+
+    protected Client getClient() {
+        if (_client == null) {
+            ClientBuilder clientBuilder = ClientBuilder.newBuilder();
+
+            clientBuilder.connectTimeout(10, TimeUnit.SECONDS);
+            clientBuilder.readTimeout(60, TimeUnit.SECONDS);
+
+            _client = clientBuilder.build();
+        }
+
+        return _client;
+    }
+
+}
--- modules/contact/contact-ws/src/main/java/hr/crosig/contact/rest/application/utils/ContactApplicationConstants.java
+++ modules/contact/contact-ws/src/main/java/hr/crosig/contact/rest/application/utils/ContactApplicationConstants.java
@@ -7,7 +7,8 @@ public class ContactApplicationConstants {
 
 	public static final String DATE_FORMAT = "yyyy-mm-dd";
 
-	public static final String MOCK_CONTACTS_API_URL =
-		"http://demo3197060.mockable.io/contacts-mock/search";
+    public static final String MOCK_CONTACTS_API_URL = "https://demo3197060.mockable.io/contacts-mock/search";
+    public static final String MOCK_CITIES_ALL_API_URL = "https://demo3197060.mockable.io/cities-mock/all";
+    public static final String MOCK_CITIES_STREET_API_URL = "https://demo3197060.mockable.io/cities-mock/streets";
 
 }
