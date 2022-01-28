64cf2065efe27e0ae16207bab0b47733c91fbb36
mmazurky <mmazurky@gmail.com>
2022-01-28 16:58:03 -0300

    CAP-135 Create module for the Cache Management

---
+++ modules/common/common-web/src/main/java/hr/crosig/common/cache/management/application/list/CacheManagementPanelCategory.java
@@ -0,0 +1,40 @@
+package hr.crosig.common.cache.management.application.list;
+
+import com.liferay.application.list.BasePanelCategory;
+import com.liferay.application.list.PanelCategory;
+import com.liferay.application.list.constants.PanelCategoryKeys;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import hr.crosig.common.cache.management.constants.CacheManagementPanelCategoryKeys;
+import org.osgi.service.component.annotations.Component;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+/**
+ * @author marcelo.mazurky
+ */
+@Component(
+        immediate = true,
+        property = {
+                "panel.category.key=" + PanelCategoryKeys.SITE_ADMINISTRATION,
+                "panel.category.order:Integer=100"
+        },
+        service = PanelCategory.class
+)
+public class CacheManagementPanelCategory extends BasePanelCategory {
+
+    @Override
+    public String getKey() {
+        return CacheManagementPanelCategoryKeys.CONTROL_PANEL_CATEGORY;
+    }
+
+    @Override
+    public String getLabel(Locale locale) {
+        ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+                "content.Language", locale, getClass());
+
+        return LanguageUtil.get(resourceBundle, "category.agent.portal");
+    }
+
+}
---
+++ modules/common/common-web/src/main/java/hr/crosig/common/cache/management/constants/CacheManagementPortletKeys.java
@@ -0,0 +1,10 @@
+package hr.crosig.common.cache.management.constants;
+
+/**
+ * @author marcelo.mazurky
+ */
+public class CacheManagementPortletKeys {
+
+    public static final String CACHE_MANAGEMENT_PORTLET = "hr_crosig_common_cache_management_portlet_CacheManagementPortlet";
+
+}
