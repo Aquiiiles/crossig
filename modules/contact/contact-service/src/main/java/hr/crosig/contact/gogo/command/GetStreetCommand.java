4a5b618614d437e832322c5dceaae852a6ae4d69
Guilherme Kfouri <guiokfouri@gmail.com>
2022-02-01 11:41:14 -0300

    CAP-123 Create gogocommands for Street

---
+++ modules/contact/contact-service/src/main/java/hr/crosig/contact/gogo/command/GetStreetCommand.java
@@ -0,0 +1,32 @@
+package hr.crosig.contact.gogo.command;
+
+import hr.crosig.contact.service.CityLocalService;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import java.util.Arrays;
+
+/**
+ * @author Guilherme Kfouri
+ */
+@Component(
+	property = {"osgi.command.function=getCity", "osgi.command.scope=blade"},
+	service = Object.class
+)
+public class GetStreetCommand {
+
+	public void getCity(String cityName) {
+		try {
+			System.out.println(
+				_cityLocalService.searchCitiesNamesByName(cityName, -1, -1));
+		}
+		catch (Exception exception) {
+			System.out.println(exception.getMessage());
+			System.out.println(Arrays.toString(exception.getStackTrace()));
+		}
+	}
+
+	@Reference
+	private CityLocalService _cityLocalService;
+
+}
