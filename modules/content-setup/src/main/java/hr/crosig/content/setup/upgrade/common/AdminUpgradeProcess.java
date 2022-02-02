ece4928764534c7625dae2c8812367ca5659d63d
Victor Catanante <victor.catanante@gs.liferay.com>
2022-02-02 17:11:13 -0300

    CAP-140 Add the AdminUpgradeProcess class to solve the addPortletId permission issue

---
+++ modules/content-setup/src/main/java/hr/crosig/content/setup/upgrade/common/AdminUpgradeProcess.java
@@ -0,0 +1,51 @@
+package hr.crosig.content.setup.upgrade.common;
+
+import com.liferay.portal.kernel.model.Role;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.model.role.RoleConstants;
+import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.service.RoleLocalServiceUtil;
+import com.liferay.portal.kernel.service.UserLocalServiceUtil;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.util.PortalUtil;
+
+/**
+ * @author victor.catanante
+ */
+public abstract class AdminUpgradeProcess extends UpgradeProcess {
+
+    @Override
+    protected void doUpgrade() throws Exception {
+        PermissionChecker originalPermissionChecker = PermissionThreadLocal.getPermissionChecker();
+        String originalName = PrincipalThreadLocal.getName();
+
+        try {
+            long companyId = PortalUtil.getDefaultCompanyId();
+
+            Role adminRole = RoleLocalServiceUtil.getRole(companyId, RoleConstants.ADMINISTRATOR);
+
+            User adminUser = UserLocalServiceUtil.getRoleUsers(
+                    adminRole.getRoleId()
+            ).get(
+                    0
+            );
+
+            PrincipalThreadLocal.setName(adminUser.getUserId());
+
+            PermissionChecker adminPermissionChecker = PermissionCheckerFactoryUtil.create(adminUser);
+
+            PermissionThreadLocal.setPermissionChecker(adminPermissionChecker);
+
+            doUpgradeAsAdmin();
+        }
+        finally {
+            PrincipalThreadLocal.setName(originalName);
+            PermissionThreadLocal.setPermissionChecker(originalPermissionChecker);
+        }
+    }
+
+    protected abstract void doUpgradeAsAdmin() throws Exception;
+}
