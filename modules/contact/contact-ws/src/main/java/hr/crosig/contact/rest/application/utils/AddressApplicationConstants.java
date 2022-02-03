e811f6168cd65ee26d3204e7aab452931ab249dc
mmazurky <mmazurky@gmail.com>
2022-02-03 16:21:17 -0300

    CAP-139 Alter the mockable path to the API's pattern

--- modules/contact/contact-ws/src/main/java/hr/crosig/contact/rest/application/utils/AddressApplicationConstants.java
+++ modules/contact/contact-ws/src/main/java/hr/crosig/contact/rest/application/utils/AddressApplicationConstants.java
@@ -4,11 +4,6 @@ package hr.crosig.contact.rest.application.utils;
  * @author marcelo.mazurky
  */
 public class AddressApplicationConstants {
-
-	public static final String MOCK_CITIES_ALL_API_URL =
-		"https://demo3197060.mockable.io/cities-mock/all";
-
-	public static final String MOCK_CITIES_STREET_API_URL =
-		"https://demo3197060.mockable.io/cities-mock/streets";
-
+    public static final String MOCK_CITIES_ALL_API_URL = "https://demo3197060.mockable.io/api/city";
+    public static final String MOCK_CITIES_STREET_API_URL = "https://demo3197060.mockable.io/api/city/1/streets";
 }
