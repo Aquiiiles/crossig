4d1b61c2c8f66789e6563642f2916ec5fec8014d
Leonardo Miyagi <leonardo.miyagi@gs.liferay.com>
2022-02-03 15:04:35 -0300

    CAP-144 Create IDIT Client Util

---
+++ modules/common/common-ws-idit-client/src/main/java/hr/crosig/common/ws/idit/client/IDITWSClientUtil.java
@@ -0,0 +1,37 @@
+package hr.crosig.common.ws.idit.client;
+
+import hr.crosig.common.ws.exception.ServiceInvocationException;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.util.tracker.ServiceTracker;
+
+import java.util.HashMap;
+
+/**
+ * @author Leonardo Miyagi
+ */
+public class IDITWSClientUtil {
+
+    public static IDITWSClient getService() {
+        return _serviceTracker.getService();
+    }
+
+    public static String search() throws ServiceInvocationException {
+        return getService().searchContact(new HashMap<>());
+    }
+
+    private static ServiceTracker<IDITWSClient, IDITWSClient>
+        _serviceTracker;
+
+    static {
+        Bundle bundle = FrameworkUtil.getBundle(IDITWSClient.class);
+
+        ServiceTracker<IDITWSClient, IDITWSClient> serviceTracker =
+                new ServiceTracker<>(
+                        bundle.getBundleContext(), IDITWSClient.class, null);
+
+        serviceTracker.open();
+
+        _serviceTracker = serviceTracker;
+    }
+}
