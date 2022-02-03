16ff02149aa5ff78ad9326a554b8368c5dba629e
Otavio Cornelio <otavio.silva@gs.liferay.com>
2022-02-03 16:56:34 -0300

    CAP-115 Add and change language keys

--- modules/proposal/proposal-web/src/constants/languageKeys.ts
+++ modules/proposal/proposal-web/src/constants/languageKeys.ts
@@ -10,12 +10,15 @@ export const CONTACT_SEARCH_FIELD_AREA_CODE= Liferay.Language.get('contact-searc
 export const CONTACT_SEARCH_FIELD_PHONE_NUMBER= Liferay.Language.get('contact-search-field-phone-number');
 export const CONTACT_SEARCH_FIELD_EMAIL_ADDRESS= Liferay.Language.get('contact-search-field-email-address');
 export const CONTACT_SEARCH_CREATE_NEW_CONTACT= Liferay.Language.get('contact-search-create-new-contact');
-export const CREATE_NEW_CONTACT_TITLE= Liferay.Language.get('create-new-contact-title');
-export const CREATE_NEW_CONTACT_SUBTITLE= Liferay.Language.get('create-new-contact-subtitle');
-export const CREATE_NEW_CONTACT_BASIC_INFO_TITLE= Liferay.Language.get('create-new-contact-basic-info-title');
-
-
-
-
-
-
+export const CREATE_NEW_CONTACT = {
+    TITLE: Liferay.Language.get('create-new-contact-title'),
+    SUBTITLE: Liferay.Language.get('create-new-contact-subtitle'),
+    BASIC_INFO_TITLE: Liferay.Language.get('create-new-contact-basic-info-title'),
+    FIELD: {
+        FIRST_NAME: Liferay.Language.get('create-new-contact-field-first-name'),
+        LAST_NAME: Liferay.Language.get('create-new-contact-field-last-name'),
+        BIRTH_DATE: Liferay.Language.get('create-new-contact-field-birth-date'),
+        OIB: Liferay.Language.get('create-new-contact-field-oib'),
+        FOREIGNER_STATUS: Liferay.Language.get('create-new-contact-foreigner-status')
+    }
+}
--- modules/proposal/proposal-web/src/views/NewContact/index.tsx
+++ modules/proposal/proposal-web/src/views/NewContact/index.tsx
@@ -1,18 +1,15 @@
 import React from "react";
 import { Content, Wrapper } from "./styles";
-import {
-  CREATE_NEW_CONTACT_TITLE,
-  CREATE_NEW_CONTACT_SUBTITLE,
-} from "../../constants/languageKeys";
+import { CREATE_NEW_CONTACT } from "../../constants/languageKeys";
 import ContactInfo from "./containers/ContactInfo";
 
 const NewContact: React.FC = () => {
   return (
     <Wrapper>
       <Content id="NewContact-main-container">
-        <h3>{CREATE_NEW_CONTACT_TITLE}</h3>
+        <h3>{CREATE_NEW_CONTACT.TITLE}</h3>
         <p style={{ marginBottom: "1.875rem" }}>
-          {CREATE_NEW_CONTACT_SUBTITLE}
+          {CREATE_NEW_CONTACT.SUBTITLE}
         </p>
         <ContactInfo />
       </Content>
