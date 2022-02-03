01a3acc3cddf725b5aef90f02485ed804e1293b3
Guilherme Kfouri <guiokfouri@gmail.com>
2022-02-03 17:47:30 -0300

    CAP-116 Add Addresses to ContactInfo

--- modules/proposal/proposal-web/src/views/NewContact/containers/ContactInfo/index.tsx
+++ modules/proposal/proposal-web/src/views/NewContact/containers/ContactInfo/index.tsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import { Wrapper } from "./style";
 import BasicInfo from "./components/molecules/BasicInfo";
+import Addresses from "./components/molecules/Addresses"
+import { cities } from "../../../../constants/contactConstants"
 import { Provider as ContactInfoProvider } from "react-redux";
 import { CREATE_NEW_CONTACT } from "../../../../constants/languageKeys";
 import store from "./contactStore";
 
 const ContactInfo: React.FC = () => {
+  const [contactType, setContactType] = useState(1)
+
   return (
     <ContactInfoProvider store={store}>
       <Wrapper id="ContactInfo-main-container">
@@ -14,9 +18,10 @@ const ContactInfo: React.FC = () => {
           {CREATE_NEW_CONTACT.SUBTITLE}
         </p>
         <BasicInfo />
+        <Addresses contactType={contactType} cities={cities} />
       </Wrapper>
     </ContactInfoProvider>
   );
 };
 
-export default ContactInfo;
+export default ContactInfo
