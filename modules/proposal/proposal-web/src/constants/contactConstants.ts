3acdb2e92f7bd716fbd75ee38e67956be35fcc6f
Guilherme Kfouri <guiokfouri@gmail.com>
2022-02-03 17:47:11 -0300

    CAP-116 Create contact constants

---
+++ modules/proposal/proposal-web/src/constants/contactConstants.ts
@@ -0,0 +1,28 @@
+export const ContactType = {
+    "Individual": 1,
+    "Self-Employed": 2,
+    "Legal Entity": 3
+}
+
+export const cities = [
+    {
+        label: "Alemanha",
+        value: 1
+    },
+    {
+        label: "Brasil",
+        value: 2
+    },
+    {
+        label: "Croatia",
+        value: 3
+    },
+    {
+        label: "EUA",
+        value: 4
+    },
+    {
+        label: "Inglaterra",
+        value: 5
+    }
+]
