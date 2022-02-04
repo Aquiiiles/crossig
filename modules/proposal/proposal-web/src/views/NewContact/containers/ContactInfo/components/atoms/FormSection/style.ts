3a5a1e210cc255e30fd1a75c78be267b80cabaae
Otavio Cornelio <otavio.silva@gs.liferay.com>
2022-02-04 11:31:29 -0300

    CAP-115 Style changes

--- modules/proposal/proposal-web/src/views/NewContact/containers/ContactInfo/components/atoms/FormSection/style.ts
+++ modules/proposal/proposal-web/src/views/NewContact/containers/ContactInfo/components/atoms/FormSection/style.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.section`
-  padding: 1rem 1rem 1rem 2rem;
+  padding: 1rem 1rem 2rem 2rem;
 `;
--- modules/proposal/proposal-web/src/views/NewContact/containers/ContactInfo/style.ts
+++ modules/proposal/proposal-web/src/views/NewContact/containers/ContactInfo/style.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div`
+export const Wrapper = styled.form`
   background-color: #fff;
   padding: 1rem 0;
   box-shadow: rgb(0 0 0 / 10%) 0px 2px 5px;
