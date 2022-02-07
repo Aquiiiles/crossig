7f1f62f6a3acf102c117cb8866ba2271a87ad779
Victor Catanante <victor.catanante@gs.liferay.com>
2022-02-07 16:19:44 -0300

    CAP-117 Remove unnecessary properties from the Button style

--- modules/proposal/proposal-web/src/views/ContactInfoForm/containers/Form/components/atoms/CreateContactButton/index.tsx
+++ modules/proposal/proposal-web/src/views/ContactInfoForm/containers/Form/components/atoms/CreateContactButton/index.tsx
@@ -1,5 +1,4 @@
 import React, { MouseEventHandler } from "react";
-
 import { Button } from "./styles";
 
 interface props {
--- modules/proposal/proposal-web/src/views/ContactInfoForm/containers/Form/components/atoms/CreateContactButton/styles.ts
+++ modules/proposal/proposal-web/src/views/ContactInfoForm/containers/Form/components/atoms/CreateContactButton/styles.ts
@@ -1,11 +1,7 @@
 import styled from "styled-components";
 import ClayButton from "@clayui/button";
 
-
 export const Button = styled(ClayButton)`
-  position: absolute;
-  right: 1px;
-  bottom: 1px;
   border: none;
   color: #f6f7f7;
   background-color: #051226 !important;
