6b8a76c05a9b059284d1c409fa2a8a64edf2e079
Victor Catanante <victor.catanante@gs.liferay.com>
2022-02-07 11:00:29 -0300

    CAP-117 Use the SectionTitle component in the ContactInfoFields molecule

--- modules/proposal/proposal-web/src/views/ContactInfo/containers/Form/components/molecules/ContactInfoFields/index.tsx
+++ modules/proposal/proposal-web/src/views/ContactInfo/containers/Form/components/molecules/ContactInfoFields/index.tsx
@@ -1,8 +1,13 @@
 import { Wrapper } from "./styles";
+import SectionTitle from "../../../../../../../shared/atoms/SectionTitle";
+import {
+  CONTACT_INFO_TITLE
+} from "../../../../../../../constants/languageKeys";
 
 const ContactInfoFields: React.FC = () =>  {
   return (
     <Wrapper>
+      <SectionTitle title={CONTACT_INFO_TITLE} />
     </Wrapper>
   );
 };
