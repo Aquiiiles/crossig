662e36428d816135c0f9a46c1e1d142f4d8b0271
Victor Catanante <victor.catanante@gs.liferay.com>
2022-02-07 13:32:24 -0300

    CAP-117 Create the CreateContactButton component

---
+++ modules/proposal/proposal-web/src/views/ContactInfo/containers/Form/components/atoms/CreateContactButton/index.tsx
@@ -0,0 +1,18 @@
+import React, { MouseEventHandler } from "react";
+
+import { Button } from "./styles";
+
+interface props {
+  handleClick: MouseEventHandler;
+}
+
+const CreateContactButton: React.FC<props> = ({ handleClick }: props) => {
+  return (
+    <Button 
+      onClick={handleClick}>
+      Create Contact
+    </Button>
+  );
+};
+
+export default CreateContactButton;
