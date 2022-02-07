40ea0c8d7e4615840ae035b1ac65625ba81e60c4
Victor Catanante <victor.catanante@gs.liferay.com>
2022-02-07 16:17:01 -0300

    CAP-117 Remove unused style files

--- modules/proposal/proposal-web/src/views/ContactInfoForm/containers/Form/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Wrapper = styled.div `
-    padding: 1.8125rem 2.6875rem;
-    min-height:24.125rem;
-    width:100%;
-    background-color:#fff;
-    box-shadow: 0 2px 5px rgba(0,0,0,0.7);
-`;
