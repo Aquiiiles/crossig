4bda17d7accf241befdf62dced7d600638401de6
Victor Catanante <victor.catanante@gs.liferay.com>
2022-02-07 16:17:36 -0300

    CAP-117 Remove containers from the ContactInfoForm component

--- modules/proposal/proposal-web/src/views/ContactInfoForm/containers/Form/index.tsx
+++ modules/proposal/proposal-web/src/views/ContactInfoForm/containers/Form/index.tsx
@@ -1,8 +1,6 @@
 import ContactInfoFields from "./components/molecules/ContactInfoFields";
-import { Wrapper } from "./styles";
 
 const Form: React.FC = () => {
-
   return (
     <ContactInfoFields />
   );
--- modules/proposal/proposal-web/src/views/ContactInfoForm/index.tsx
+++ modules/proposal/proposal-web/src/views/ContactInfoForm/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Form from "./containers/Form";
-import { Content, Wrapper } from "./styles";
 
 const ContactInfo: React.FC = () => {
   return (<Form />);
