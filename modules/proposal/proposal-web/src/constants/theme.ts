df164103bcdc0d06cb27e41a6c81a70239ecc65a
Otavio Cornelio <otavio.silva@gs.liferay.com>
2022-02-07 17:28:05 -0300

    CAP-100 Create styled-components theme

---
+++ modules/proposal/proposal-web/src/constants/theme.ts
@@ -0,0 +1,46 @@
+import { DefaultTheme } from "styled-components";
+
+const defaultTheme: DefaultTheme = {
+  color: {
+    primary: {
+      main: "#112D82",
+      dark: "#102872",
+      light: "#0C3D9D",
+      links: "#005CD0",
+      linksLight: "#93BBFF",
+    },
+    neutral: {
+      black: "#000",
+      white: "#FFF",
+      background: "#F2F3F4",
+      dividerGrey: "#DEDFE0",
+      neutralGrey: "#95A0AB",
+      neutralGreyText: "#79838C",
+    },
+    secondary: {
+      1: "#F49F0A",
+      2: "#F7B900",
+      3: "#F5D306",
+      4: "#CEAE00",
+      5: "#008C2C",
+      6: "#9AD2FE",
+      7: "#B03600",
+      8: "#F86381",
+      9: "#FFBBC0",
+    },
+    action: {
+      default: "#00CB79",
+      hover: "#1A5CCC",
+      active: "#2172FF",
+      navbarLink: "#92A3D7",
+      disabled: "#D4D9DE",
+    },
+    feedback: {
+      success: "#00CB79",
+      warning: "#FFCF56",
+      error: "#FF0000",
+    },
+  },
+};
+
+export default defaultTheme;
