684fce7f6500bc83b95b9d256089fee4056d4830
Guilherme Kfouri <guiokfouri@gmail.com>
2022-02-08 18:03:00 -0300

    CAP-116 Insert divider line

--- modules/proposal/proposal-web/src/views/NewContact/containers/ContactInfo/components/molecules/Addresses/index.tsx
+++ modules/proposal/proposal-web/src/views/NewContact/containers/ContactInfo/components/molecules/Addresses/index.tsx
@@ -8,6 +8,7 @@ import ClayForm, {
 import { contactTypes } from "../../../../../../../constants/contactConstants";
 import { CREATE_NEW_CONTACT } from "../../../../../../../constants/languageKeys";
 import { useContactSelector } from "../../../contactStore";
+import { Line } from "./styles";
 
 const Addresses: React.FC<{ countries: Array<Object> }> = ({ countries }) => {
 	const [sameAddress, setSameAdress] = useState(true);
@@ -16,83 +17,32 @@ const Addresses: React.FC<{ countries: Array<Object> }> = ({ countries }) => {
 	);
 
 	return (
-		<FormSection title={CREATE_NEW_CONTACT.ADDRESS_TITLE}>
-			<SectionSubTitle
-				subTitle={
-					contactType === contactTypes.Individual
-						? CREATE_NEW_CONTACT.ID_ADDRES
-						: CREATE_NEW_CONTACT.REGISTERED_OFFICE_ADDRESS
-				}
-			/>
+		<>
+			<Line />
+			<FormSection title={CREATE_NEW_CONTACT.ADDRESS_TITLE}>
+				<SectionSubTitle
+					subTitle={
+						contactType === contactTypes.Individual
+							? CREATE_NEW_CONTACT.ID_ADDRES
+							: CREATE_NEW_CONTACT.REGISTERED_OFFICE_ADDRESS
+					}
+				/>
 
-			<Row half>
-				<label htmlFor='contactTypeInput'>Type</label>
-				<ClaySelectWithOption options={countries} />
-			</Row>
+				<Row half>
+					<ClayForm.Group>
+						<label htmlFor='country'>{CREATE_NEW_CONTACT.FIELD.COUNTRY}</label>
+						<ClaySelectWithOption id='country' options={countries} required />
+					</ClayForm.Group>
+				</Row>
 
-			<Row>
-				<ClayForm.Group>
-					<ClayInput.Group>
-						<ClayInput.GroupItem>
-							<label htmlFor='city'>{CREATE_NEW_CONTACT.FIELD.CITY}</label>
-
-							<ClayInput
-								id='city'
-								aria-required={true}
-								type='text'
-								required={true}
-							/>
-						</ClayInput.GroupItem>
-
-						<ClayInput.GroupItem>
-							<label htmlFor='postal-code'>
-								{CREATE_NEW_CONTACT.FIELD.POSTAL_CODE}
-							</label>
-
-							<ClayInput id='postal-code' type='text' />
-						</ClayInput.GroupItem>
-					</ClayInput.Group>
-				</ClayForm.Group>
-			</Row>
-
-			<ClayForm.Group>
-				<label htmlFor='street-address'>
-					{CREATE_NEW_CONTACT.FIELD.STREET_ADDRESS}
-				</label>
-
-				<ClayInput id='street-address' type='text' required={true} />
-			</ClayForm.Group>
-
-			<ClayForm.Group>
-				<label htmlFor='house-number'>
-					{CREATE_NEW_CONTACT.FIELD.HOUSE_NUMBER}
-				</label>
-
-				<ClayInput id='house-number' type='text' />
-			</ClayForm.Group>
-
-			<SectionSubTitle subTitle={CREATE_NEW_CONTACT.DISPATCH_ADDRESS} />
-
-			<ClayCheckbox
-				checked={sameAddress}
-				onChange={() => setSameAdress((val) => !val)}
-				label={CREATE_NEW_CONTACT.FIELD.DISPATCH_ADDRESS}
-			/>
-
-			{!sameAddress && (
-				<>
-					<strong>{CREATE_NEW_CONTACT.FIELD.COUNTRY}</strong>
-					<ClaySelectWithOption options={countries} />
-
+				<Row>
 					<ClayForm.Group>
 						<ClayInput.Group>
 							<ClayInput.GroupItem>
-								<label htmlFor='dispatch-city'>
-									{CREATE_NEW_CONTACT.FIELD.CITY}
-								</label>
+								<label htmlFor='city'>{CREATE_NEW_CONTACT.FIELD.CITY}</label>
 
 								<ClayInput
-									id='dispatch-city'
+									id='city'
 									aria-required={true}
 									type='text'
 									required={true}
@@ -100,33 +50,98 @@ const Addresses: React.FC<{ countries: Array<Object> }> = ({ countries }) => {
 							</ClayInput.GroupItem>
 
 							<ClayInput.GroupItem>
-								<label htmlFor='dispatch-postal-code'>
+								<label htmlFor='postal-code'>
 									{CREATE_NEW_CONTACT.FIELD.POSTAL_CODE}
 								</label>
 
-								<ClayInput id='dispatch-postal-code' type='text' />
+								<ClayInput id='postal-code' type='text' />
 							</ClayInput.GroupItem>
 						</ClayInput.Group>
 					</ClayForm.Group>
+				</Row>
 
+				<ClayForm.Group>
+					<label htmlFor='street-address'>
+						{CREATE_NEW_CONTACT.FIELD.STREET_ADDRESS}
+					</label>
+
+					<ClayInput id='street-address' type='text' required={true} />
+				</ClayForm.Group>
+
+				<Row half>
 					<ClayForm.Group>
-						<label htmlFor='dispatch-street-address'>
-							{CREATE_NEW_CONTACT.FIELD.STREET_ADDRESS}
-						</label>
-
-						<ClayInput id='dispatch-street-address' type='text' />
-					</ClayForm.Group>
-
-					<ClayForm.Group>
-						<label htmlFor='dispatch-house-number'>
+						<label htmlFor='house-number'>
 							{CREATE_NEW_CONTACT.FIELD.HOUSE_NUMBER}
 						</label>
 
-						<ClayInput id='dispatch-house-number' type='text' />
+						<ClayInput id='house-number' type='text' />
 					</ClayForm.Group>
-				</>
-			)}
-		</FormSection>
+				</Row>
+
+				<SectionSubTitle subTitle={CREATE_NEW_CONTACT.DISPATCH_ADDRESS} />
+
+				<ClayCheckbox
+					checked={sameAddress}
+					onChange={() => setSameAdress((val) => !val)}
+					label={CREATE_NEW_CONTACT.FIELD.DISPATCH_ADDRESS}
+				/>
+
+				{!sameAddress && (
+					<>
+						<Row half>
+							<strong>{CREATE_NEW_CONTACT.FIELD.COUNTRY}</strong>
+							<ClaySelectWithOption options={countries} />
+						</Row>
+
+						<Row half>
+							<ClayForm.Group>
+								<ClayInput.Group>
+									<ClayInput.GroupItem>
+										<label htmlFor='dispatch-city'>
+											{CREATE_NEW_CONTACT.FIELD.CITY}
+										</label>
+
+										<ClayInput
+											id='dispatch-city'
+											aria-required={true}
+											type='text'
+											required={true}
+										/>
+									</ClayInput.GroupItem>
+
+									<ClayInput.GroupItem>
+										<label htmlFor='dispatch-postal-code'>
+											{CREATE_NEW_CONTACT.FIELD.POSTAL_CODE}
+										</label>
+
+										<ClayInput id='dispatch-postal-code' type='text' />
+									</ClayInput.GroupItem>
+								</ClayInput.Group>
+							</ClayForm.Group>
+						</Row>
+
+						<ClayForm.Group>
+							<label htmlFor='dispatch-street-address'>
+								{CREATE_NEW_CONTACT.FIELD.STREET_ADDRESS}
+							</label>
+
+							<ClayInput id='dispatch-street-address' type='text' />
+						</ClayForm.Group>
+
+						<Row half>
+							<ClayForm.Group>
+								<label htmlFor='dispatch-house-number'>
+									{CREATE_NEW_CONTACT.FIELD.HOUSE_NUMBER}
+								</label>
+
+								<ClayInput id='dispatch-house-number' type='text' />
+							</ClayForm.Group>
+						</Row>
+					</>
+				)}
+			</FormSection>
+			<Line />
+		</>
 	);
 };
 
