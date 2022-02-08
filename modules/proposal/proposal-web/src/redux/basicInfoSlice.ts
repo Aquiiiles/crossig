3ec29aad5197efdd4bda531946756f90dfb114a3
Otavio Cornelio <otavio.silva@gs.liferay.com>
2022-02-08 17:56:02 -0300

    CAP-163 Better alphabetic/numeric only fields

--- modules/proposal/proposal-web/src/redux/basicInfoSlice.ts
+++ modules/proposal/proposal-web/src/redux/basicInfoSlice.ts
@@ -22,29 +22,29 @@ const basicInfoSlice = createSlice({
       state.contactType = action.payload;
     },
     setFirstName(state, action: PayloadAction<string>) {
-      state.firstName = action.payload;
+      state.firstName = action.payload.replaceAll(/\d/g, "");
     },
     setLastName(state, action: PayloadAction<string>) {
-      state.lastName = action.payload;
+      state.lastName = action.payload.replaceAll(/\d/g, "");
     },
     setDateDay(state, action: PayloadAction<string>) {
-      if (!isNaN(Number(action.payload)) && action.payload.length <= 2) {
-        state.dateDay = action.payload;
+      if (action.payload.length <= 2) {
+        state.dateDay = action.payload.replaceAll(/\D/g, "");
       }
     },
     setDateMonth(state, action: PayloadAction<string>) {
-      if (!isNaN(Number(action.payload)) && action.payload.length <= 2) {
-        state.dateMonth = action.payload;
+      if (action.payload.length <= 2) {
+        state.dateMonth = action.payload.replaceAll(/\D/g, "");
       }
     },
     setDateYear(state, action: PayloadAction<string>) {
-      if (!isNaN(Number(action.payload)) && action.payload.length <= 4) {
-        state.dateYear = action.payload;
+      if (action.payload.length <= 4) {
+        state.dateYear = action.payload.replaceAll(/\D/g, "");
       }
     },
     setOIB(state, action: PayloadAction<string>) {
-      if (!isNaN(Number(action.payload)) && action.payload.length <= 11) {
-        state.oib = action.payload;
+      if (action.payload.length <= 11) {
+        state.oib = action.payload.replaceAll(/\D/g, "");
       }
     },
     toggleForeignerStatus(state) {
