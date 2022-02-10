fcbc519c2b087881018f7f7505fc5c1d8289e09a
Guilherme Kfouri <guiokfouri@gmail.com>
2022-02-10 11:28:52 -0300

    CAP-191 Fix AddressApplication tests

--- modules/contact/contact-ws/src/main/java/hr/crosig/contact/rest/application/utils/TestConstants.java
+++ modules/contact/contact-ws/src/main/java/hr/crosig/contact/rest/application/utils/TestConstants.java
@@ -1,15 +1,15 @@
 package hr.crosig.contact.rest.application.utils;
 
 import hr.crosig.common.ws.exception.ServiceInvocationException;
+import hr.crosig.contact.dto.CityDTO;
 import hr.crosig.contact.dto.ContactDTO;
 import hr.crosig.contact.dto.EmailDTO;
 import hr.crosig.contact.dto.SearchDTO;
 import hr.crosig.contact.dto.TelephoneDTO;
 
+import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.List;
-
-import javax.ws.rs.core.Response;
 
 /**
  * @author marcelo.mazurky
@@ -17,6 +17,12 @@ import javax.ws.rs.core.Response;
 public class TestConstants {
 
 	// api response constants
+
+	public static final int API_BAD_REQUEST_STATUS_CODE =
+			Response.Status.BAD_REQUEST.getStatusCode();
+
+	public static final String API_BAD_REQUEST_STATUS_CONTENT =
+			Response.Status.BAD_REQUEST.getReasonPhrase();
 
 	public static final int API_ERROR_STATUS_CODE =
 		Response.Status.INTERNAL_SERVER_ERROR.getStatusCode();
@@ -38,14 +44,23 @@ public class TestConstants {
 
 	public static final long VALID_CITY_ID = 50;
 
+	public static final String VALID_CITY_NAME = "SOKOLOVAC";
+
 	public static final ContactDTO VALID_CONTACT_REQUEST = new ContactDTO();
 
 	public static final String VALID_GET_CONTACT_REQUEST = "10";
 
 	public static final SearchDTO VALID_SEARCH_REQUEST = new SearchDTO();
 
+	public static final String VALID_STREET_NAME = "VUGRINŠČAK";
+
+	public static final List<CityDTO> validCity = new ArrayList<>();
+
 	public static final List<EmailDTO> validEmailRequest = new ArrayList<>();
+
 	public static final List<TelephoneDTO> validPhoneNumberRequest =
 		new ArrayList<>();
+
+	public static final List<String> validStreet = new ArrayList<>();
 
 }
