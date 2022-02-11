1dc5f545ef11b40c15538c84eabb5adc6c4bf17b
Guilherme Kfouri <guiokfouri@gmail.com>
2022-02-11 10:17:09 -0300

    CAP-195 Implement clear streets

--- modules/common/common-web/src/main/java/hr/crosig/common/cache/management/command/action/ClearCacheMVCActionCommand.java
+++ modules/common/common-web/src/main/java/hr/crosig/common/cache/management/command/action/ClearCacheMVCActionCommand.java
@@ -6,17 +6,15 @@ import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
 import com.liferay.portal.kernel.servlet.SessionErrors;
 import com.liferay.portal.kernel.util.ParamUtil;
-
 import hr.crosig.common.cache.management.constants.CacheManagementCommandNames;
 import hr.crosig.common.cache.management.constants.CacheManagementPortletKeys;
 import hr.crosig.common.cache.management.constants.ClearCacheMVCActionConstants;
 import hr.crosig.contact.service.IndexManagementLocalService;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
-
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author marcelo.mazurky
@@ -51,6 +49,11 @@ public class ClearCacheMVCActionCommand extends BaseMVCActionCommand {
 					actionRequest, "cityName");
 
 				_indexManagementLocalService.clearCityByName(cityName);
+			}
+			else if (ClearCacheMVCActionConstants.CLEAR_CACHE_STREET.equals(
+					cacheName)) {
+
+				_indexManagementLocalService.clearStreets();
 			}
 		}
 		catch (Exception exception) {
