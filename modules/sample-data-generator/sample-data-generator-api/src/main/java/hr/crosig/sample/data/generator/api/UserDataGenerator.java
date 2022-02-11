4274066bfd0b1ff43532faf671f7c00220515135
mmazurky <mmazurky@gmail.com>
2022-02-11 10:01:36 -0300

    CAP-175 Add Sample Data Generator's API Module

---
+++ modules/sample-data-generator/sample-data-generator-api/src/main/java/hr/crosig/sample/data/generator/api/UserDataGenerator.java
@@ -0,0 +1,14 @@
+package hr.crosig.sample.data.generator.api;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.UserEmailAddressException;
+import com.liferay.portal.kernel.model.User;
+
+import java.sql.SQLException;
+
+/**
+ * @author marcelo.mazurky
+ */
+public interface UserDataGenerator {
+    public void addRegularUser(String firstName, String lastName, String emailAddress, String password, String userGroupName) throws PortalException, SQLException;
+}
