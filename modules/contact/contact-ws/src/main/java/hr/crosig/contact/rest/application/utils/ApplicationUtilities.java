e145ca2af9de877a33013363dea0c626ebe130e0
dvdmartini <david.martini@gs.liferay.com>
2022-02-17 15:42:26 -0300

    CAP-210 Chenge method handleErrorResponse

--- modules/contact/contact-ws/src/main/java/hr/crosig/contact/rest/application/utils/ApplicationUtilities.java
+++ modules/contact/contact-ws/src/main/java/hr/crosig/contact/rest/application/utils/ApplicationUtilities.java
@@ -10,6 +10,7 @@ import com.google.gson.Gson;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 
+import com.liferay.portal.kernel.util.Validator;
 import hr.crosig.common.ws.response.ServiceResponse;
 
 import java.util.Map;
@@ -24,127 +25,144 @@ import javax.ws.rs.core.Response;
  */
 public class ApplicationUtilities {
 
-	/**
-	 * Creates an Entity Json in String by an Entity Map
-	 * @param entity
-	 * @return
-	 */
-	public static String createEntityJsonString(Map<String, Object> entity) {
-		Gson gson = new Gson();
+    /**
+     * Creates an Entity Json in String by an Entity Map
+     *
+     * @param entity
+     * @return
+     */
+    public static String createEntityJsonString(Map<String, Object> entity) {
+        Gson gson = new Gson();
 
-		return gson.toJson(entity);
-	}
+        return gson.toJson(entity);
+    }
 
-	/**
-	 * Creates an Entity Json in String by an Entity Object
-	 * @param entity
-	 * @return
-	 */
-	public static String createEntityJsonString(Object entity)
-		throws JsonProcessingException {
+    /**
+     * Creates an Entity Json in String by an Entity Object
+     *
+     * @param entity
+     * @return
+     */
+    public static String createEntityJsonString(Object entity)
+            throws JsonProcessingException {
 
-		ObjectMapper mapper = _configureJsonMapper();
+        ObjectMapper mapper = _configureJsonMapper();
 
-		return mapper.writeValueAsString(entity);
-	}
+        return mapper.writeValueAsString(entity);
+    }
 
-	/**
-	 * Gets the Default HTTP Client
-	 * @return
-	 */
-	public static synchronized Client getDefaultHttpClient() {
-		ClientBuilder clientBuilder = ClientBuilder.newBuilder();
+    /**
+     * Gets the Default HTTP Client
+     *
+     * @return
+     */
+    public static synchronized Client getDefaultHttpClient() {
+        ClientBuilder clientBuilder = ClientBuilder.newBuilder();
 
-		clientBuilder.connectTimeout(10, TimeUnit.SECONDS);
-		clientBuilder.readTimeout(60, TimeUnit.SECONDS);
+        clientBuilder.connectTimeout(10, TimeUnit.SECONDS);
+        clientBuilder.readTimeout(60, TimeUnit.SECONDS);
 
-		return clientBuilder.build();
-	}
+        return clientBuilder.build();
+    }
 
-	/**
-	 * Handles the Error Response by Exception
-	 * @param e
-	 * @return
-	 */
-	public static Response handleErrorResponse(Exception exception) {
-		_log.error(exception);
+    /**
+     * Handles the Error Response by Exception
+     *
+     * @param e
+     * @return
+     */
+    public static Response handleErrorResponse(Exception exception) {
+        _log.error(exception);
 
-		return Response.status(
-			Response.Status.INTERNAL_SERVER_ERROR
-		).entity(
-			Response.Status.INTERNAL_SERVER_ERROR.getReasonPhrase()
-		).build();
-	}
+        return Response.status(
+                Response.Status.INTERNAL_SERVER_ERROR
+        ).entity(
+                Response.Status.INTERNAL_SERVER_ERROR.getReasonPhrase()
+        ).build();
+    }
 
-	/**
-	 * Handles Error Response by Status Code and Response
-	 * @param statusCode
-	 * @param response
-	 * @return
-	 */
-	public static Response handleErrorResponse(
-		int statusCode, String response) {
+    /**
+     * Handles Error Response by Status Code and Response
+     *
+     * @param statusCode
+     * @param response
+     * @return
+     */
+    public static Response handleErrorResponse(
+            int statusCode, String response) {
 
-		return Response.status(
-			statusCode
-		).entity(
-			Response.Status.fromStatusCode(
-				statusCode
-			).getReasonPhrase()
-		).build();
-	}
+        if (Validator.isNotNull(response)) {
+            _log.error(response);
+        }
 
-	/**
-	 * Handles the Service Response
-	 * @param serviceResponse
-	 * @return
-	 */
-	public static Response handleServiceResponse(
-		ServiceResponse serviceResponse) {
+        Response.Status status = Response.Status.fromStatusCode(statusCode);
 
-		boolean success = false;
+        String resp = status != null && status.getReasonPhrase() != null ? status.getReasonPhrase() : Response.Status.INTERNAL_SERVER_ERROR.getReasonPhrase();
 
-		if (serviceResponse.getCode() == 200) {
-			success = true;
-		}
+        return Response.status(
+                statusCode
+        ).entity(
+                resp
+        ).build();
+    }
 
-		if (success) {
-			return handleSuccessResponse(serviceResponse.getContent());
-		}
 
-		return handleErrorResponse(
-			serviceResponse.getCode(), serviceResponse.getContent());
-	}
+    /**
+     * Handles the Service Response
+     *
+     * @param serviceResponse
+     * @return
+     */
+    public static Response handleServiceResponse(
+            ServiceResponse serviceResponse) {
 
-	/**
-	 * Handles the Success Response
-	 * @param response
-	 * @return
-	 */
-	public static Response handleSuccessResponse(String response) {
-		return Response.status(
-			Response.Status.OK
-		).entity(
-			response
-		).build();
-	}
+        boolean success = false;
 
-	/**
-	 * Configures the Json Mapper
-	 * @return
-	 */
-	private static ObjectMapper _configureJsonMapper() {
-		ObjectMapper mapper = new ObjectMapper();
+        if (serviceResponse.getCode() == 200) {
+            success = true;
+        }
 
-		mapper.configure(
-			JsonReadFeature.ALLOW_UNESCAPED_CONTROL_CHARS.mappedFeature(),
-			true);
-		mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        if (success) {
+            return handleSuccessResponse(serviceResponse.getContent());
+        }
 
-		return mapper;
-	}
+        return handleErrorResponse(
+                serviceResponse.getCode(), serviceResponse.getContent());
+    }
 
-	private static final Log _log = LogFactoryUtil.getLog(
-		ApplicationUtilities.class);
+
+
+    /**
+     * Handles the Success Response
+     *
+     * @param response
+     * @return
+     */
+    public static Response handleSuccessResponse(String response) {
+        return Response.status(
+                Response.Status.OK
+        ).entity(
+                response
+        ).build();
+    }
+
+    /**
+     * Configures the Json Mapper
+     *
+     * @return
+     */
+    private static ObjectMapper _configureJsonMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+
+        mapper.configure(
+                JsonReadFeature.ALLOW_UNESCAPED_CONTROL_CHARS.mappedFeature(),
+                true);
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+        return mapper;
+    }
+
+    private static final Log _log = LogFactoryUtil.getLog(
+            ApplicationUtilities.class);
 
 }
