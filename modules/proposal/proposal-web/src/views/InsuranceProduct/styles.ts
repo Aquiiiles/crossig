951004ff3369b59698ac3f2a91786733b0e132e9
Otavio Cornelio <otavio.silva@gs.liferay.com>
2022-03-03 10:55:55 -0300

    CAP-178 Adapt InsuranceProduct to Stepper

--- modules/proposal/proposal-web/src/views/InsuranceProduct/styles.ts
+++ modules/proposal/proposal-web/src/views/InsuranceProduct/styles.ts
@@ -6,6 +6,10 @@ export const Wrapper = styled.div`
   background-color: ${(props) => props.theme.color.neutral.background};
 `;
 
+export const InnerWrapper = styled.div`
+  flex: 1 1 auto;
+`;
+
 export const Content = styled.div`
   flex: 1;
   padding: 6.25rem 3.5rem 1.25rem 3.75rem;
@@ -26,5 +30,5 @@ export const Products = styled.div`
 export const Footer = styled.div`
   display: flex;
   border-top: 1px solid ${(props) => props.theme.color.neutral.dividerGrey};
-  padding: 3.125rem 0 2.8125rem 0;
+  padding: 3.125rem 0 2.8125rem 3.75rem;
 `;
