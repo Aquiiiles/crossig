053135d7f5a85dce580c3cb1261a9e051fc6c654
Otavio Cornelio <otavio.silva@gs.liferay.com>
2022-03-09 11:27:12 -0300

    CAP-302 Add functionality to mobile results

--- modules/proposal/proposal-web/src/views/ContactSearch/containers/SearchResult/components/atoms/ResultMobile/index.tsx
+++ modules/proposal/proposal-web/src/views/ContactSearch/containers/SearchResult/components/atoms/ResultMobile/index.tsx
@@ -7,6 +7,7 @@ import ClayButton from "@clayui/button";
 import {
   CONTACT_SEARCH_TABLE_USE_CONTACT,
   CONTACT_SEARCH_TABLE_VIEW_DETAILS,
+  ROLES_ON_POLICY,
 } from "../../../../../../../constants/languageKeys";
 import MissingInformationIcon from "../MissingInformationIcon";
 
@@ -15,7 +16,13 @@ interface Props {
 }
 
 const ResultMobile: React.FC<Props> = ({ contact }) => {
-  const [, , types] = useTableRowState();
+  const [
+    ,
+    { formatDOB, openUpdateContact, addContact },
+    types,
+    history,
+    dispatch,
+  ] = useTableRowState();
 
   return (
     <Wrapper>
@@ -26,7 +33,7 @@ const ResultMobile: React.FC<Props> = ({ contact }) => {
           <h6 className="h10">{constants.SUB_NAME}</h6>
           <p className="body-small">{contact[constants.SUB_KEY]}</p>
           <h6 className="h10">{constants.DOB_NAME}</h6>
-          <p className="body-small">{contact[constants.DOB_KEY]}</p>
+          <p className="body-small">{formatDOB(contact[constants.DOB_KEY])}</p>
           <h6 className="h10">{constants.NAME_NAME}</h6>
           <p className="body-small">{contact[constants.NAME_KEY]}</p>
         </InnerGrid>
@@ -42,10 +49,28 @@ const ResultMobile: React.FC<Props> = ({ contact }) => {
         </InnerGrid>
       </MainGrid>
       <ButtonGroup>
-        <ClayButton displayType="secondary" className="ghost">
+        <ClayButton
+          displayType="secondary"
+          className="ghost"
+          onClick={() => openUpdateContact(contact[constants.EXT_NUMBER_KEY])}
+        >
           {CONTACT_SEARCH_TABLE_VIEW_DETAILS}
         </ClayButton>
-        <ClayButton displayType="primary">
+        <ClayButton
+          displayType="primary"
+          onClick={() => {
+            dispatch(
+              addContact({
+                [constants.EXT_NUMBER_KEY]: contact[constants.EXT_NUMBER_KEY],
+                [constants.OIB_KEY]: contact[constants.OIB_KEY],
+                [constants.SUB_KEY]: contact[constants.SUB_KEY],
+                [constants.NAME_KEY]: contact[constants.NAME_KEY],
+                [constants.ROLES_KEY]: [ROLES_ON_POLICY.INSURED],
+              })
+            );
+            history.push("/product");
+          }}
+        >
           {CONTACT_SEARCH_TABLE_USE_CONTACT}
         </ClayButton>
       </ButtonGroup>
