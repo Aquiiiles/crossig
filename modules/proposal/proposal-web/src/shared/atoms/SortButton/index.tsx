3b8ab8006ff8b9eb19ff6d7d5136b98fcf1347e1
Otavio Cornelio <otavio.silva@gs.liferay.com>
2022-03-14 09:59:54 -0300

    CAP-295 Fix sort button issues with dropdown

--- modules/proposal/proposal-web/src/shared/atoms/SortButton/index.tsx
+++ modules/proposal/proposal-web/src/shared/atoms/SortButton/index.tsx
@@ -2,14 +2,17 @@ import React from "react";
 import { Button } from "./styles";
 import { ReactComponent as FilterIcon } from "../../../assets/filterIcon.svg";
 
-const SortButton: React.FC<React.ButtonHTMLAttributes<HTMLButtonElement>> = (
-  props
-) => {
-  return (
-    <Button displayType="unstyled" {...props}>
-      <FilterIcon />
-    </Button>
-  );
-};
+type PropsType = React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+const SortButton = React.forwardRef<HTMLButtonElement, PropsType>(
+  (props, ref) => {
+    return (
+      <Button displayType="unstyled" ref={ref} {...props}>
+        <FilterIcon />
+      </Button>
+    );
+  }
+);
+SortButton.displayName = "SortButton";
 
 export default SortButton;
