4abb23d8b22f33bc4002fd5c914f3a8309b6612c
Guilherme Kfouri <guiokfouri@gmail.com>
2022-03-18 13:39:46 -0300

    CAP-334 Implement create and update proposal

--- modules/proposal/proposal-service/src/main/java/hr/crosig/proposal/service/impl/ProposalLocalServiceImpl.java
+++ modules/proposal/proposal-service/src/main/java/hr/crosig/proposal/service/impl/ProposalLocalServiceImpl.java
@@ -16,6 +16,8 @@ package hr.crosig.proposal.service.impl;
 
 import com.liferay.portal.aop.AopService;
 
+import hr.crosig.proposal.dto.ProposalDTO;
+import hr.crosig.proposal.model.Proposal;
 import hr.crosig.proposal.service.base.ProposalLocalServiceBaseImpl;
 
 import org.osgi.service.component.annotations.Component;
@@ -28,4 +30,65 @@ import org.osgi.service.component.annotations.Component;
 	service = AopService.class
 )
 public class ProposalLocalServiceImpl extends ProposalLocalServiceBaseImpl {
+
+	public ProposalDTO createProposal(ProposalDTO proposalDTO) {
+		long proposalId = counterLocalService.increment(
+			Proposal.class.getName());
+
+		Proposal proposal = proposalPersistence.create(proposalId);
+
+		proposal = _updateProposal(proposalDTO, proposal);
+
+		return _mapToDTO(proposal);
+	}
+
+	public ProposalDTO updateProposal(ProposalDTO proposalDTO) {
+		Proposal proposal = proposalPersistence.fetchByPrimaryKey(
+			proposalDTO.getProposalId());
+
+		proposal = _updateProposal(proposalDTO, proposal);
+
+		return _mapToDTO(proposal);
+	}
+
+	private ProposalDTO _mapToDTO(Proposal proposal) {
+		ProposalDTO proposalDTO = new ProposalDTO();
+
+		proposalDTO.setAgentUserId(proposal.getAgentUserId());
+		proposalDTO.setCompanyId(proposal.getCompanyId());
+		proposalDTO.setCreateDate(proposal.getCreateDate());
+		proposalDTO.setExternalProposalNumber(
+			proposal.getExternalProposalNumber());
+		proposalDTO.setInsuredObjectExtNumber(
+			proposal.getInsuredObjectExtNumber());
+		proposalDTO.setLastUpdate(proposal.getLastUpdate());
+		proposalDTO.setModifiedDate(proposal.getModifiedDate());
+		proposalDTO.setOrigin(proposal.getOrigin());
+		proposalDTO.setPolicyHolderExtNumber(
+			proposal.getPolicyHolderExtNumber());
+		proposalDTO.setProposalId(proposal.getProposalId());
+		proposalDTO.setStatus(proposal.getStatus());
+		proposalDTO.setUserId(proposal.getUserId());
+		proposalDTO.setUserName(proposal.getUserName());
+
+		return proposalDTO;
+	}
+
+	private Proposal _updateProposal(
+		ProposalDTO proposalDTO, Proposal proposal) {
+
+		proposal.setExternalProposalNumber(
+			proposalDTO.getExternalProposalNumber());
+		proposal.setLastUpdate(proposalDTO.getLastUpdate());
+		proposal.setOrigin(proposalDTO.getOrigin());
+		proposal.setAgentUserId(proposalDTO.getAgentUserId());
+		proposal.setPolicyHolderExtNumber(
+			proposalDTO.getPolicyHolderExtNumber());
+		proposal.setInsuredObjectExtNumber(
+			proposalDTO.getInsuredObjectExtNumber());
+		proposal.setStatus(proposalDTO.getStatus());
+
+		return proposalPersistence.update(proposal);
+	}
+
 }
