77483ad438612e1ef4bed4c0e0d24e04bad1a363
Guilherme Kfouri <guiokfouri@gmail.com>
2022-03-22 16:26:28 -0300

    CAP-352 Basic info tests

--- modules/proposal/proposal-web/src/__tests__/NewContact.tests.tsx
+++ modules/proposal/proposal-web/src/__tests__/NewContact.tests.tsx
@@ -6,6 +6,7 @@ import { Provider as StoreProvider } from "react-redux";
 import { ThemeProvider } from "styled-components";
 import defaultTheme from "../constants/theme";
 import store from "../redux/store";
+import { act } from "react-dom/test-utils";
 
 const App: React.FC = () => {
   return (
@@ -17,10 +18,22 @@ const App: React.FC = () => {
   );
 };
 
+const mockHistoryPush = jest.fn();
+const mockHistoryReplace = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useHistory: () => ({
+    push: mockHistoryPush,
+    replace: mockHistoryReplace,
+  }),
+}));
+
 describe("New Contact page", () => {
   beforeEach(() => {
     render(<App />);
   });
+
   afterEach(cleanup);
 
   it("Should render the New Contact page", () => {
@@ -32,5 +45,115 @@ describe("New Contact page", () => {
 
   it("Cancel button should be active", () => {
     expect(screen.getByText(language.CONTACT_INFO.CANCEL)).not.toBeDisabled();
+  });
+
+  it("Click cancel button", () => {
+    act(() => screen.getByText(language.CONTACT_INFO.CANCEL).click());
+
+    expect(mockHistoryReplace).toHaveBeenCalledWith({
+      pathname: "/",
+      state: { doSearch: true },
+    });
+  });
+
+  it("Contact types in dropdown", () => {
+    act(() => screen.getByText(language.CREATE_NEW_CONTACT.TYPE).click());
+
+    expect(
+      screen.getByText(
+        language.CREATE_NEW_CONTACT.FIELD.CONTACT_TYPE.INDIVIDUAL
+      )
+    ).toBeVisible();
+    expect(
+      screen.getByText(
+        language.CREATE_NEW_CONTACT.FIELD.CONTACT_TYPE.SELF_EMPLOYED
+      )
+    ).toBeVisible();
+    expect(
+      screen.getByText(
+        language.CREATE_NEW_CONTACT.FIELD.CONTACT_TYPE.LEGAL_ENTITY
+      )
+    ).toBeVisible();
+  });
+
+  it("Individual type fields", () => {
+    act(() => {
+      screen.getByText(language.CREATE_NEW_CONTACT.TYPE).click();
+      screen
+        .getByText(language.CREATE_NEW_CONTACT.FIELD.CONTACT_TYPE.INDIVIDUAL)
+        .click();
+    });
+
+    expect(
+      screen.getByText(language.CREATE_NEW_CONTACT.FIELD.FIRST_NAME)
+    ).toBeVisible();
+    expect(
+      screen.getByText(language.CREATE_NEW_CONTACT.FIELD.LAST_NAME)
+    ).toBeVisible();
+    expect(
+      screen.getByText(language.CREATE_NEW_CONTACT.FIELD.BIRTH_DATE)
+    ).toBeVisible();
+    expect(
+      screen.getByText(language.CREATE_NEW_CONTACT.FIELD.OIB)
+    ).toBeVisible();
+    expect(
+      screen.getByText(language.CREATE_NEW_CONTACT.FIELD.FOREIGNER_STATUS)
+    ).toBeChecked();
+  });
+
+  it("Self Employed type fields", () => {
+    act(() => {
+      screen.getByText(language.CREATE_NEW_CONTACT.TYPE).click();
+      screen
+        .getByText(language.CREATE_NEW_CONTACT.FIELD.CONTACT_TYPE.SELF_EMPLOYED)
+        .click();
+    });
+
+    expect(
+      screen.getByText(language.CREATE_NEW_CONTACT.FIELD.COMPANY_NAME)
+    ).toBeVisible();
+    expect(
+      screen.getByText(language.CREATE_NEW_CONTACT.FIELD.LAST_NAME)
+    ).toBeVisible();
+    expect(
+      screen.getByText(language.CREATE_NEW_CONTACT.FIELD.SUBSIDIARY_NUMBER)
+    ).toBeDisabled();
+    expect(
+      screen.getByText(language.CREATE_NEW_CONTACT.FIELD.OIB)
+    ).toBeVisible();
+    expect(
+      screen.getByText(language.CREATE_NEW_CONTACT.FIELD.FOREIGNER_STATUS)
+    ).not.toBeChecked();
+    expect(
+      screen.getByText(language.CREATE_NEW_CONTACT.SUBMIT_BUTTON)
+    ).toBeDisabled();
+  });
+
+  it("Legal type fields", () => {
+    act(() => {
+      screen.getByText(language.CREATE_NEW_CONTACT.TYPE).click();
+      screen
+        .getByText(language.CREATE_NEW_CONTACT.FIELD.CONTACT_TYPE.SELF_EMPLOYED)
+        .click();
+    });
+
+    expect(
+      screen.getByText(language.CREATE_NEW_CONTACT.FIELD.COMPANY_NAME)
+    ).toBeVisible();
+    expect(
+      screen.getByText(language.CREATE_NEW_CONTACT.FIELD.LAST_NAME)
+    ).toBeVisible();
+    expect(
+      screen.getByText(language.CREATE_NEW_CONTACT.FIELD.SUBSIDIARY_NUMBER)
+    ).toBeDisabled();
+    expect(
+      screen.getByText(language.CREATE_NEW_CONTACT.FIELD.OIB)
+    ).toBeVisible();
+    expect(
+      screen.getByText(language.CREATE_NEW_CONTACT.FIELD.FOREIGNER_STATUS)
+    ).not.toBeChecked();
+    expect(
+      screen.getByText(language.CREATE_NEW_CONTACT.SUBMIT_BUTTON)
+    ).toBeDisabled();
   });
 });
